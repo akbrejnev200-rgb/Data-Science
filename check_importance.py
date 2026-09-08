15474520e6a047de9d9ec1d3a34d8dd05095093e
Koffi Brejnev Akoumani <akbrejnev200@gmail.com>
2026-09-08 16:09:24 +0200

    Ajout du SQL de feature engineering graphe et du script de verification des importances

---
+++ check_importance.py
@@ -0,0 +1,13 @@
+﻿import sys, joblib, pandas as pd
+sys.path.insert(0, "src")
+from features import FEATURES
+
+m = joblib.load("models/xgboost_model.joblib")
+s = pd.Series(m.feature_importances_, index=FEATURES).sort_values(ascending=False)
+
+print(s.head(12).to_string())
+
+vol = ["nb_transactions_envoyees", "nb_transactions_recues", "max_transactions_jour"]
+print()
+print("Part des 2 features de volume brut : {:.1%}".format(s[vol[:2]].sum()))
+print("En ajoutant max_transactions_jour  : {:.1%}".format(s[vol].sum()))
